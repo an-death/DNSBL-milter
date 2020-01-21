f24b45cf71bd4c433601f972c8de996ca796b79d
Aleksey Simuskov <opiumofthepeople@yandex.ru>
2020-01-21 18:26:25 +0300

    Added Prometheus metrics as exporter result

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import System.Environment (getArgs)
 
-import Data.ByteString (ByteString)
+import Data.ByteString (ByteString, hGetLine)
 import Data.ByteString.Char8 (unpack)
+import Data.ByteString.Lazy (hPut)
+import Data.ByteString.Lazy.Char8 (pack)
+import qualified Data.Text as T (pack, replace, toLower)
+
+import Data.Either (rights)
 
 import Control.Concurrent (forkIO)
 import qualified Control.Concurrent.Chan.Unagi as U
@@ -14,26 +21,44 @@ import qualified Network.Simple.TCP as TCP (HostPreference(Host), serve)
 import Network.Socket (socketToHandle)
 import System.IO
   ( BufferMode(NoBuffering)
-  , IOMode(ReadWriteMode)
   , Handle
+  , IOMode(ReadWriteMode)
   , hClose
   , hSetBuffering
   )
 
+import Milter (newMilter)
 import Network.DNSBL
 
-import Milter (newMilter)
+import qualified Network.Wai.Middleware.Prometheus as P
+import qualified Prometheus as P
+import qualified Prometheus.Metric.GHC as P
 
+import Network.HTTP.Types (status200, status404)
+import qualified Network.Wai as Wai
+import Network.Wai.Handler.Warp (run)
+
+name :: String
 name = "DNSBL-milter"
 
 main :: IO ()
 main = do
-  [_, host, _, port] <- getArgs
+  host:port:rblProviders <- getArgs
   print $ "Start " ++ name ++ " on " ++ host ++ ":" ++ port
   (inCn, outCn) <- U.newChan
-  forkIO $ dnsBLResolve outCn ["zen.spamhaus.org"] 
+  metrics <- registerMetrics
+  forkIO $ dnsBLResolve metrics outCn rblProviders
+  forkIO $ run 6000 (P.prometheus P.def app)
   let handler = newMilter (U.writeChan inCn)
+--  let handler = \hdl -> hGetLine hdl >>= U.writeChan inCn
   server host port handler
+
+app :: Wai.Application
+app request respond = do
+  response <- case Wai.pathInfo request of
+      [] -> return $ Wai.responseLBS status200 [("Content-Type", "text/plain")] (pack name)
+      _  -> return $ Wai.responseLBS status404 [("Content-Type", "text/plain")] "sorry"
+  respond response
 
 server :: String -> String -> (Handle -> IO ()) -> IO ()
 server host port handler =
@@ -52,12 +77,49 @@ server host port handler =
       hClose hdl
       putStrLn $ "connection done" ++ show remoteAddr
 
-dnsBLResolve :: U.OutChan ByteString -> [String] -> IO ()
-dnsBLResolve cn providers= loop
+dnsBLResolve :: Metrics -> U.OutChan ByteString -> [String] -> IO ()
+dnsBLResolve metric chan providers = loop
   where
     loop = do
-      ip <- U.readChan cn
-      c <- withRBLProviders providers (unpack ip) 
-      print c
+      ip <- U.readChan chan
+      incTotal metric ()
+      c <- withRBLProviders providers (unpack ip)
+      case rights c of
+        [] -> return ()
+        provider -> do
+          incBlacklisted metric ()
+          mapM_ (incBlacklist metric) $ map T.pack provider
       loop
 
+data Metrics =
+  Metrics
+    { incTotal :: () -> IO ()
+    , incBlacklisted :: () -> IO ()
+    , incBlacklist :: P.Label1 -> IO ()
+    }
+
+registerMetrics = do
+  _ <- P.register P.ghcMetrics
+  _total <- P.register total
+  _blacklisted <- P.register blacklisted
+  _blacklist <- P.register blacklist
+  let inc = const . P.incCounter
+  return $
+    Metrics (inc _total) (inc _blacklisted) (\p -> P.withLabel _blacklist p P.incCounter)
+
+total :: P.Metric P.Counter
+total =
+  P.counter $ P.Info (withname "total_check") "The number of checked IP's."
+
+blacklisted :: P.Metric P.Counter
+blacklisted =
+  P.counter $
+  P.Info (withname "blacklisted") "The count of blacklisted IP's from total."
+
+blacklist :: P.Metric (P.Vector P.Label1 P.Counter)
+blacklist =
+  P.vector (withname "blacklist") $
+  P.counter $
+  P.Info (withname "blacklist") "The count of matched IP's by provider."
+
+withname descr = T.toLower . T.replace "-" "_" . T.pack $ name ++ "_" ++ descr
